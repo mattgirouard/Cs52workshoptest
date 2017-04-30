74da23a7d10fef783b802b90ca8666c599d0117c
mgirouard23 <mattgirouard2013@yahoo.com>
2017-04-30 19:37:38 -0400

    test

--- api.py
+++ api.py
@@ -108,7 +108,7 @@ class AllEvents(Resource):
             conn = mysql.connect()
             cursor = conn.cursor()
 
-            cursor.execute("""SELECT * from users""")
+            cursor.execute("""SELECT * FROM users""")
             data = cursor.fetchall()
 
             return data
