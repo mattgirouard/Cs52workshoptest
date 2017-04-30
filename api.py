e1ac09508e9072cc634f119f8ba8db84fc0438f7
mgirouard23 <mattgirouard2013@yahoo.com>
2017-04-30 17:08:53 -0400

    test to database

--- api.py
+++ api.py
@@ -3,15 +3,14 @@ from flask_restful import Resource, Api
 from flask_restful import reqparse
 from flask.ext.mysql import MySQL
 
-
 mysql = MySQL()
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'jay'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
-app.config['MYSQL_DATABASE_DB'] = 'ItemListDb'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'b1ba3ee4b6964c'
+app.config['MYSQL_DATABASE_PASSWORD'] = '91b1dd37'
+app.config['MYSQL_DATABASE_DB'] = 'heroku_567546a5d8d43a6'
+app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-iron-east-03.cleardb.net'
 
 
 mysql.init_app(app)
@@ -19,16 +18,16 @@ mysql.init_app(app)
 api = Api(app)
 
 class CreateUser(Resource):
-    def post(self):
+    def get(self):
         try:
             # Parse the arguments
-            parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address to create user')
-            parser.add_argument('password', type=str, help='Password to create user')
-            args = parser.parse_args()
+            # parser = reqparse.RequestParser()
+            # parser.add_argument('email', type=str, help='Email address to create user')
+            # parser.add_argument('password', type=str, help='Password to create user')
+            # args = parser.parse_args()
 
-            _userEmail = args['email']
-            _userPassword = args['password']
+            _userEmail = 'test'
+            _userPassword = 'testPass'
 
             conn = mysql.connect()
             cursor = conn.cursor()
